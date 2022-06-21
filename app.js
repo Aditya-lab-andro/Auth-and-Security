4afb7930ab97d90df741e66cfea253347b2d832b
Aditya <addyaditya745@gmail.com>
2022-06-21 13:12:05 +0530

    Use decrypt technique to secure password using 10 rounds of salting

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -39,29 +40,40 @@ app.get("/logout",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    if(!err){
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save((err)=>{
+       if(err)
+        console.log(err);
+       else
+         res.render("secrets");
+    });
+  }
   });
-  newUser.save((err)=>{
-     if(err)
-      console.log(err);
-     else
-       res.render("secrets");
-  });
+
+
+
 });
 
 app.post("/login",(req,res)=>{
   const email = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email:email},(err,foundUser)=>{
     if(err)
      console.log(err);
     else{
       if(foundUser){
-        if(foundUser.password === password)
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(result===true)
+           res.render("secrets");
+        });
+
       }
     }
 
